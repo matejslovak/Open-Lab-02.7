35025d6dbf0eabd67fab3432b259bed84158e59b
matejslovak <joggurtik1@gmail.com>
2019-10-28 09:02:06 +0100

    wf

---
+++ Open-Lab-02.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Open_Lab_02._7
+{
+    class Program
+    {
+        public static bool IsTimeForMilkAndCookies(int month, int day)
+        {
+            if (month == 12 && day == 24)
+            {
+                return true;
+            }
+            else
+            {
+                return !true;
+            }
+        }
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Enter day");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter month");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(IsTimeForMilkAndCookies(b, a));
+        }
+    }
+}
+
